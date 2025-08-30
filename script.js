097b73b6bd1e4bafb4e359462fd495621a8f98b6
Azhaan <bossshadab51@gmail.com>
2025-08-30 13:47:30 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Initialize the Animate On Scroll (AOS) library
+AOS.init({
+    duration: 800, // Animation duration in milliseconds
+    once: true,    // Whether animation should happen only once - while scrolling down
+    offset: 100,   // Offset (in px) from the original trigger point
+});
